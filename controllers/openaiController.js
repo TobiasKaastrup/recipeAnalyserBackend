cca914f1e368d26dde9270eeaf69cfa9f7aea294
tokn <tokn@itu.dk>
2024-11-06 17:31:40 +0100

    hardcoded prompt

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -4,7 +4,16 @@ const generateMeta = async (req, res) => {
     const { prompt } = req.body
     const recipe = await openai.chat.completions.create({
         model: "gpt-4o-mini",
-        messages: JSON.parse(prompt),
+        messages: [
+            {
+                role: "system",
+                content: "You are a cooking recipe generator. The user will give you a URL to an online cooking recipe. Your initial job is to analyze the dish and give it a rating of 1-10 and these parameters 1) Spiciness, 2), Richness ,3) Salt level, 4) Acidity, 5) The abstraction level of the language in the recipe, 6) The texture of the dish (smooth -> crispy), 7) Complexity (skill level required by the cook), 8) Speed (how long does it take to took)"
+            },
+            {
+                role: "user",
+                content: 'https://ottolenghi.co.uk/pages/recipes/puttanesca-style-salmon-bake',
+            },
+        ],
     })
 
     res.status(200).json({
