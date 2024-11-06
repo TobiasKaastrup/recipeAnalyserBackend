0bb741ef9a71e770506660935868b5af778ccb64
tokn <tokn@itu.dk>
2024-11-06 16:34:12 +0100

    simplify

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,16 +1,28 @@
 const openai = require('../config/openaiConfig')
 
 const generateMeta = async (req, res) => {
-    const { prompt } = req.body
+    try{
+        const { prompt } = req.body
     const recipe = await openai.chat.completions.create({
         model: "gpt-4o-mini",
-        messages: prompt,
+        messages: [
+            {
+                role: "system",
+                content: "You are a cooking recipe generator. Your job is to come up with a recipe that includes the ingredients that the user inputs. The user may also input a cooking technique or another constraint you should incorporate in the recipe.  You should always include a header with a recipe name, a list of ingredients as well as a series of steps to cook the dish. You are free to add any other ingredients that you think might add to the final dish" },
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
     })
+    }
+    catch(e){
+        console.log(e)
+    }
 
     res.status(200).json({
         recipe: recipe.choices[0].message
     })
-    
 }
 
 const generateImage = async (req, res) => {
